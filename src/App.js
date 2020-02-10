8187b0042eb53a82d778e10146362a7ec285626c
lubeina <52536093+lubeina@users.noreply.github.com>
2020-02-10 17:03:31 +0300

    Task done

--- src/App.js
+++ src/App.js
@@ -82,6 +82,27 @@ function App() {
                   </div>
                 </div>
               </div>
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="card">
+                  <div className="image">
+                    <img
+                      className="card-img-top img-fluid"
+                      src={authors[3].imageUrl}
+                      alt={authors[3].first_name + " " + authors[3].last_name}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <span>
+                        {authors[3].first_name + " " + authors[3].last_name}
+                      </span>
+                    </h5>
+                    <small className="card-text">
+                      {authors[3].books.length + " books"}
+                    </small>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
